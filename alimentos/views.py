078b233855f61aed450de69b3b5a26f76b01caf3
joaodir <joaodirceuconstantinocarvalho@gmail.com>
2025-05-28 13:34:23 -0300

    alt 28/05 - alimentos

--- alimentos/views.py
+++ alimentos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from alimentos.models import Classificacao, Alimento, Nutriente
 from django.http import JsonResponse as js
 from django.contrib import messages
+from django.core.paginator import Paginator
+from django.conf import settings
 
 # NUTRIENTES
 def nutrientes(request):
@@ -64,3 +66,63 @@ def inserir_classificacao(req):
 def alimentos(request):
    alimentos = Alimento.objects.all()
    return render(request, 'alimentos.html', {"alimentos": alimentos})
+
+def busca_alimento_nome(request):
+   if request.GET.get('nome'):
+      alimento = Alimento.objects.filter(nome=request.GET.get('nome'))
+      if not alimento.exists():
+         return js({'alimentos': 'Não existe'})
+      return js({'alimentos': list(alimento.values())})
+   return js({'alimento': 'Informe um nome'})
+
+def inserir_alimento(request):
+    if request.GET.get('nome') and request.GET.get('id_classificacao'):
+        nome = request.GET.get('nome')
+        id_classificacao = request.GET.get('id_classificacao')
+
+        alimento_existente = Alimento.objects.filter(nome=nome)
+        if not alimento_existente.exists():
+            try:
+                classificacao = Classificacao.objects.get(id=id_classificacao)
+
+                Alimento.objects.create(nome=nome, classificacao_id=classificacao.id)
+                return js({'alimento': f'{nome} adicionado com sucesso!'})
+            except Classificacao.DoesNotExist:
+                return js({'alimento': 'Classificação informada não existe'})
+        return js({'alimento': 'Alimento já existente'})
+    return js({'alimento': 'Informe nome e id_classificacao'})
+
+def atualizar_alimento(request):
+    if request.GET.get('id') and request.GET.get('nome'):
+        id_alimento = request.GET.get('id')
+        nome = request.GET.get('nome')
+        id_classificacao = request.GET.get('id_classificacao')
+        
+        try:
+            alimento = Alimento.objects.get(id_alimento=id_alimento)
+            if alimento.nome != nome and Alimento.objects.filter(nome=nome).exists():
+                return js({'alimento': 'Esse nome de alimento já existe'})
+            
+            alimento.nome = nome
+            if id_classificacao:
+                try:
+                    classificacao = Classificacao.objects.get(id_classificacao=id_classificacao)
+                    alimento.id_classificacao = classificacao
+                except Classificacao.DoesNotExist:
+                    return js({'alimento': 'Classificação informada não existe'})
+            alimento.save()
+            return js({'alimento': 'Alimento atualizado com sucesso'})
+        except Alimento.DoesNotExist:
+            return js({'alimento': 'Alimento não encontrado'})
+    return js({'alimento': 'Preciso de um id e um nome'})
+
+def apagar_alimento(request):
+    if request.GET.get('id'):
+        id_alimento = request.GET.get('id')
+        try:
+            alimento = Alimento.objects.get(id=id_alimento)
+            alimento.delete()
+            return js({'alimento': 'DELETADO'})
+        except Alimento.DoesNotExist:
+            return js({'alimento': 'Alimento não encontrado'})
+    return js({'alimento': 'Preciso de uma id'})
