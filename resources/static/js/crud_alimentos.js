1b49b4faf8e8caaf15e77aed6ed4f96f7b92beff
Luan <luancarlos1201@gmail.com>
2025-06-04 14:51:03 -0300

    So guardano

--- resources/static/js/crud_alimentos.js
+++ resources/static/js/crud_alimentos.js
@@ -4,6 +4,7 @@ document.body.addEventListener('click', function (evento){
    const desativar_btn = evento.target.closest('.desativar-btn');
    const ativar_btn = evento.target.closest('.ativar-btn');
    const atualizar_btn = evento.target.closest('.update-btn');
+   
    if (desativar_btn) {
       evento.preventDefault()
       console.log('Id desativar capturada:', id_tr.dataset.id);
