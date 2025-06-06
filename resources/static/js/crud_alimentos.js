afc271c136c4534c097dc2a2abb1bb2638a0dc4a
Luan <luancarlos1201@gmail.com>
2025-06-05 23:10:44 -0300

    Update crud_alimentos.js

--- resources/static/js/crud_alimentos.js
+++ resources/static/js/crud_alimentos.js
@@ -1,18 +1,29 @@
 // criei um escutador que espera um evento de click no body
 document.body.addEventListener('click', function (evento){ 
-   const desativar_btn = evento.target.closest('.desativar-btn');
-   const ativar_btn = evento.target.closest('.ativar-btn');
-   const atualizar_btn = evento.target.closest('.update-btn');
-   if (desativar_btn) {
-      evento.preventDefault()
-      console.log('Id desativar capturada:', desativar_btn.dataset.id);
-   }
-   if (ativar_btn) {
-      evento.preventDefault()
-      console.log('Id desativar capturada:', ativar_btn.dataset.id);
-   }
-   if (atualizar_btn) {
-      evento.preventDefault()
-      console.log('Id atualização capturada:', atualizar_btn.dataset.id);
-   }
+    const desativar_btn = evento.target.closest('.desativar-btn');
+    const ativar_btn = evento.target.closest('.ativar-btn');
+    const atualizar_btn = evento.target.closest('.update-btn');
+    if (desativar_btn) {
+        evento.preventDefault()
+        console.log('Permissão para desativar: ', desativar_btn.dataset.id);
+        ativar(desativar_btn)
+    }
+    if (ativar_btn) {
+        evento.preventDefault()
+        console.log('Permissão para ativar   : ', ativar_btn.dataset.id);
+    }
+    if (atualizar_btn) {
+        evento.preventDefault()
+        console.log('Permissão para atualizar: ', atualizar_btn.dataset.id);
+    }
 });
+
+function ativar(elemento){
+    const url = 'teste'
+    Swal.fire({
+        icon: 'info',
+        confirmButtonColor: ''
+    }).then(resp => {
+        console.log('Saída: '+url+resp)
+    });
+}
