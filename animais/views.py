f132918b49bda6db6ffa8c1c0671d399889b1349
Luan <luancarlos1201@gmail.com>
2025-10-01 15:11:45 -0300

    fix

--- animais/views.py
+++ animais/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from faker import Faker
+
 
 # Create your views here.
 def animais(request):
+    fake = Faker('pt_BR')
+    print(fake.name())
     return render(request, 'animais.html')
