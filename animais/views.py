00e82abe9de3c4702e342bd59e82042c850c665b
Luan <luancarlos1201@gmail.com>
2025-10-06 13:46:28 -0300

    Add Models

--- animais/views.py
+++ animais/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from faker import Faker
+from animais.models import Animal
 
 
 # Create your views here.
 def animais(request):
     fake = Faker('pt_BR')
     print(fake.name())
-    return render(request, 'animais.html')
+    animais = Animal.objects.all()
+    return render(request, 'animais.html', {'animais': animais})
