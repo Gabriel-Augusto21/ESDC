6d7720aadab090f570ffef5becd5b9da6fc2f437
Gabriel-Augusto21 <gomides.augustog@gmail.com>
2025-10-08 11:07:48 -0300

    fiz o paginator

--- animais/views.py
+++ animais/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from animais.models import Animal
+from django.core.paginator import Paginator
 
 def animais(request):
-    animais = Animal.objects.all()
-    return render(request, 'animais.html', {'animais': animais})
+    query = request.GET.get('query', '')
+    animais_lista = Animal.objects.filter(
+        nome__icontains=query
+    ).order_by('-is_active', 'nome', 'peso_vivo')  
+    paginator = Paginator(animais_lista, 10)
+    page_obj = paginator.get_page(request.GET.get('page'))
+    return render(request, 'animais.html', {'page_obj': page_obj, 'query': query})
