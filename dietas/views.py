715608859e109d278c5e9ada584d328703924da6
Luan <luancarlos1201@gmail.com>
2025-10-19 23:30:16 -0300

    Update views.py

--- dietas/views.py
+++ dietas/views.py
@@ -2,18 +2,6 @@ from django.shortcuts import render
 from .models import Dieta, ComposicaoDieta
 from decimal import Decimal
 
-# def dietas(request):
-#     lista_dieta = Dieta.objects.get(pk=1)
-#     comp_dieta = ComposicaoDieta.objects.all()
-#     totais = lista_dieta.total_nutrientes_vetor()
-#     print('\n')
-#     lista_totais_fornecidos = []
-#     for t in totais:
-#         print(f"Nutriente: {t['nutriente']}, Total: {t['total']} {t['unidade']}")
-#         lista_totais_fornecidos.append(f"{t['nutriente']} - {round(t['total'], 2)} {t['unidade']}")
-#     return render(request, 'dietas.html', {'dietas': lista_dieta, 'compdieta':comp_dieta, 'lista_totais': lista_totais_fornecidos, 'alimentos': alimentos})
-from decimal import Decimal
-
 def dietas(request):
     lista_dieta = Dieta.objects.get(pk=1)
     comp_dieta = ComposicaoDieta.objects.filter(dieta=lista_dieta)
