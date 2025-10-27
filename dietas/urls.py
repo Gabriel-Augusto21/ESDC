5a3307ed276873dd05f25020e189d7b064e8b766
joaodir <joaodirceuconstantinocarvalho@gmail.com>
2025-10-27 15:24:30 -0300

    tabela dieta front

--- dietas/urls.py
+++ dietas/urls.py
@@ -6,4 +6,7 @@ app_name = 'dietas'
 urlpatterns = [
     path("dietas/", views.dietas, name="dietas"),   
     path("dieta/<int:id>/", views.dieta, name="dieta"),   
+    path("desativar_dieta/", views.desativar_dieta, name="desativar_dieta"),   
+    path("ativar_dieta/", views.ativar_dieta, name="ativar_dieta"),   
+
 ]
