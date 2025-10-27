1b1a4d1ef8a544a90733bd8cffd1e6ea4a755aab
Luan <luancarlos1201@gmail.com>
2025-10-27 18:32:07 -0300

    Ajustes de visualização da dieta

--- dietas/views.py
+++ dietas/views.py
@@ -12,48 +12,70 @@ def gerenciar_dietas(request, id):
 
     # # Totais agregados 
     # totais = lista_dieta.total_nutrientes_vetor()
-    # lista_totais_fornecidos = []
-    # for t in totais:
-    #     lista_totais_fornecidos.append(f"{t['nutriente']} - {round(t['total'], 2)} {t['unidade']}")
+    # lista_totais_fornecidos = [
+    #     f"{t['nutriente']} - {round(t['total'], 2)} {t['unidade']}" for t in totais
+    # ]
 
-    # # Nutrientes por alimento
     # alimentos_nutrientes = []
+    # todos_nutrientes = set()
+
+    # # 🔹 Coleta todos os nutrientes usados na dieta
     # for comp in comp_dieta:
-    #     nutriente_alimento = []
     #     for ca in comp.alimento.composicaoalimento_set.all():
+    #         todos_nutrientes.add(ca.nutriente.nome)
+
+    # # 🔹 Converte pra lista e aplica substituição de espaços
+    # todos_nutrientes = sorted([n.replace(' ', '_') for n in todos_nutrientes])
+
+    # # 🔹 Monta a tabela com os valores de nutrientes
+    # for comp in comp_dieta:
+    #     dados_nutrientes = {n: {'produto': 0, 'unidade': ''} for n in todos_nutrientes}
+
+    #     for ca in comp.alimento.composicaoalimento_set.all():
+    #         nome_normalizado = ca.nutriente.nome.replace(' ', '_')
     #         quantidade_nutriente_dieta = round(Decimal(ca.valor) * Decimal(comp.quantidade), 2)
-    #         nutriente_alimento.append({
-    #             'nutriente': ca.nutriente.nome,
-    #             'valor': round(Decimal(ca.valor), 2),
+
+    #         dados_nutrientes[nome_normalizado] = {
     #             'produto': quantidade_nutriente_dieta,
     #             'unidade': ca.nutriente.unidade
-    #         })
+    #         }
+
     #     alimentos_nutrientes.append({
     #         'alimento': comp.alimento.nome,
     #         'quantidade': round(comp.quantidade, 2),
-    #         'nutrientes': nutriente_alimento
+    #         'nutrientes': dados_nutrientes
     #     })
-    # print(alimentos_nutrientes)
 
     # return render(request, 'gerenciar_dietas.html', {
     #     'dietas': lista_dieta,
     #     'compdieta': comp_dieta,
     #     'lista_totais': lista_totais_fornecidos,
-    #     'alimentos_nutrientes': alimentos_nutrientes
+    #     'alimentos_nutrientes': alimentos_nutrientes,
+    #     'todos_nutrientes': todos_nutrientes,
     # })
-    dieta = ComposicaoDieta.objects.all()
-    for i in dieta:
-        print(i.alimento_id)
-        print(ComposicaoAlimento.objects.get(alimento_id=i.alimento_id))
-        # for j in i:
-        #     print(j)
+    dieta = Dieta.objects.get(pk=id)
+    comp_dieta = ComposicaoDieta.objects.filter(dieta=dieta).select_related('alimento', 'alimento__classificacao').prefetch_related('alimento__composicaoalimento_set__nutriente')
 
-        # for alimento in i.alimento_id:
-        #     print(alimento)
+    # Coletar todos os nutrientes extras de todos os alimentos da dieta
+    todos_nutrientes = set()
+    for comp in comp_dieta:
+        for ca in comp.alimento.composicaoalimento_set.all():
+            todos_nutrientes.add(ca.nutriente)
+    todos_nutrientes = sorted(list(todos_nutrientes), key=lambda n: n.nome)
+
+    # Montar um dicionário de valores de nutrientes por alimento
+    nutrientes_por_alimento = {}
+    for comp in comp_dieta:
+        nutrientes_por_alimento[comp.alimento.id] = {n.id: 0 for n in todos_nutrientes}  # 0 por padrão
+        for ca in comp.alimento.composicaoalimento_set.all():
+            nutrientes_por_alimento[comp.alimento.id][ca.nutriente.id] = ca.valor
+
     return render(request, 'gerenciar_dietas.html', {
-        'aaa': dieta
+        'dieta': dieta,
+        'comp_dieta': comp_dieta,
+        'todos_nutrientes': todos_nutrientes,
+        'nutrientes_por_alimento': nutrientes_por_alimento,
     })
-
 def dietas(request):
     query = request.GET.get('query', '')
     nutrientes_lista = Dieta.objects.filter(nome__icontains=query).order_by('-is_active', 'nome')
