d0364e468ab5f9f246d617888bcf43bdb33fdffc
Luan <luancarlos1201@gmail.com>
2025-10-27 19:02:09 -0300

    Ajustes do gerenciamento da dieta

--- dados/management/commands/inserir_dietas.py
+++ dados/management/commands/inserir_dietas.py
@@ -34,24 +34,34 @@ class Command(BaseCommand):
         ComposicaoDieta.objects.create(
             quantidade= 1.8,
             dieta = dietas[0],
-            alimento = alimentos[2]
+            alimento = alimentos[1]
         )
         ComposicaoDieta.objects.create(
             quantidade= 1.5,
             dieta = dietas[0],
-            alimento = alimentos[1]
+            alimento = alimentos[2]
         )
         
         # coposicoes relacionadas ao alimento 2
         ComposicaoDieta.objects.create(
             quantidade= 2.5,
             dieta = dietas[1],
-            alimento = alimentos[1]
+            alimento = alimentos[3]
         )
         ComposicaoDieta.objects.create(
             quantidade= 3,
             dieta = dietas[1],
-            alimento = alimentos[0]
+            alimento = alimentos[4]
+        )
+        ComposicaoDieta.objects.create(
+            quantidade= 3,
+            dieta = dietas[1],
+            alimento = alimentos[5]
+        )
+        ComposicaoDieta.objects.create(
+            quantidade= 3,
+            dieta = dietas[1],
+            alimento = alimentos[6]
         )
         
         self.stdout.write(self.style.SUCCESS("Dietas e composições de teste adicionadas com sucesso!"))
